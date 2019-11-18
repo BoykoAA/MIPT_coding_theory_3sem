5e2e3f54ca846b9a48ee219b264541e8007d29b5
BoykoAA <BoykoA.A@yandex.ru>
2019-11-18 17:36:32 +0300

    in process

--- Сyclic_codes.py
+++ Сyclic_codes.py
@@ -7,12 +7,33 @@ from sympy import div
 #####################
 q = 2
 n = 7
-coef = 1101
+coef = [1,1,0,1]
 #####################
 #####################
 #####################
 
+def get_p(coef):
+    p = 0
+    for idx_c, c in enumerate(coef):
+        if c > 0:
+            p += c * x ** idx_c
+
+    return p
+
+def division(m, p):
+    q, r = div(m, p, x)
+    pol = r.as_poly(x, domain='GF(2').args[0]
+
+    return q, r, pol
+
 x = Symbol('x')
+m = x**n - 1
+p = get_p(coef)
+
+q, r, pol  = division(m, p)
+
+if pol == 0:
+    q
 
 def Poly(p,mod):
     q,r = div(p,mod,x) #quotient and remainder polynomial division by modulus mod
@@ -26,7 +47,3 @@ m = x**7 + 1
 p = x**3 + x + 1
 
 print(Poly(p*p, m))
-
-q: 3
-n: 8
-polynomial coefficients: 221
