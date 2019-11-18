0c5962bc3b7fd088f1c8679136afe686c7f245b0
BoykoAA <BoykoA.A@yandex.ru>
2019-11-18 18:05:16 +0300

    in process

--- Сyclic_codes.py
+++ Сyclic_codes.py
@@ -20,30 +20,28 @@ def get_p(coef):
 
     return p
 
-def division(m, p):
-    q, r = div(m, p, x)
-    pol = r.as_poly(x, domain='GF(2').args[0]
+def division(m, p, q):
+    if q == 2:
+        e, r = div(m, p, x)
+        pol = r.as_poly(x, domain='GF(2)').args[0]
+    else:
+        e, r = div(m, p, x)
+        pol = r.as_poly(x, domain='GF(3)').args[0]
 
-    return q, r, pol
+    return e, r, pol
 
 x = Symbol('x')
 m = x**n - 1
 p = get_p(coef)
 
-q, r, pol  = division(m, p)
+e, r, pol  = division(m, p, q)
 
 if pol == 0:
-    q
 
-def Poly(p,mod):
-    q,r = div(p,mod,x) #quotient and remainder polynomial division by modulus mod
 
-    return r.as_poly(x,domain='GF(2)') #Z_2
 
-# m = x**8 + x**4 + x**3 + x + 1
-# p = x**6 + x**5 + x + 1
 
-m = x**7 + 1
-p = x**3 + x + 1
 
-print(Poly(p*p, m))
+
+
+
